d4fd701f64c65af0d11379004adc0ff3ac37db12
zshan1 <zshan@uvm.edu>
2018-02-15 00:33:42 -0500

    Add function to allow user play 5 more times; Avoid infinity loop in guessing part if user input char instead of int; Add check function to control the length of the input in guessing

--- HumanGame.cpp
+++ HumanGame.cpp
@@ -6,17 +6,69 @@
 #include <vector>
 #include <ctime>
 
-void HumanGame() {
+bool hint = false;
+
+void Human_game(vector<int> ansVector){
     int innumber;
     int numCorrectLoc;
     char hintstatus;
+    vector<int> your;
     int status = 0;
     int numIncorrectLoc;
+    int correct = 0;
+    int length = 1;
+
+    vector<int> control = {1, 2, 3, 4, 5, 6};
+    while(correct!=4) {
+        your.clear();
+        cout << "Please enter your guess(4 digit):" << endl;
+        cin >> innumber;
+        while(innumber>6666||innumber<1111){
+            cout<<"Wrong input"<<endl;
+            cout << "Please enter your guess(4 digit):" << endl;
+            cin.clear();
+            cin.ignore(100, '\n');
+            cin >> innumber;
+        }
+        for (int i = 1; i <= 4; i++) {
+            your.push_back(innumber % 10);
+            innumber /= 10;
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (your[i] == control[j]) {
+                    correct++;
+                }
+            }
+        }
+
+    }//check the input length of the number, and then check if input the
+    reverse(your.begin(), your.end());
+    check(your, ansVector, hint, numCorrectLoc, numIncorrectLoc);
+    while(status == 0) {
+        cout << "Do you need a hint?(Y/N) Press 'Q' to quit." << endl;
+        cin >> hintstatus;
+        if (hintstatus == 'Y') {
+            hint = true;
+            status=1;
+
+        } else if (hintstatus == 'N') {
+            hint = false;
+            status=1;
+        }else if (hintstatus == 'Q') {
+            exit(0);
+        } else {
+            cout << "I don't understand." << endl;
+        }
+    }
+}
+
+
+void HumanGame() {
+    char another;
     int numCount = 0;
-    bool hint = false;
-    vector<int> your;
+    int status =0;
     vector<int> ans;
-    vector<int> control={1,2,3,4,5,6};
     int choice;
     srand(time(NULL));//to get different 4 digit for each turn
     for (int i = 1; i <= 4; i++) {
@@ -27,42 +79,25 @@ void HumanGame() {
 //    }//print out the answer(only for developing)
     cout << endl;
     while (numCount < 5) {
-        int correct = 0;
-        status= 0;
-        while(correct!=4) {
-            your.clear();
-            cout << "Please enter your guess(4 digit):" << endl;
-            cin >> innumber;
-            for (int i = 1; i <= 4; i++) {
-                your.push_back(innumber % 10);
-                innumber /= 10;
-            }
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 6; j++) {
-                    if (your[i] == control[j]) {
-                        correct++;
-                    }
-                }
-            }
-        }
-        reverse(your.begin(), your.end());
-        check(your, ans, hint, numCorrectLoc, numIncorrectLoc);
-        while(status == 0) {
-            cout << "Do you need a hint?(Y/N)" << endl;
-            cin >> hintstatus;
-
-            if (hintstatus == 'Y') {
-                hint = true;
-                status=1;
-
-            } else if (hintstatus == 'N') {
-                hint = false;
-                status=1;
-            } else {
-                cout << "I don't understand." << endl;
-            }
-        }
+        Human_game(ans);
         numCount++;
     }
-    cout<<"All your attempts are being used, you lose! Bye!"<<endl;
+    while(status == 0) {
+        cout << "All your attempts are being used! Another five guess?(Y/N)" << endl;
+        cin >> another;
+        if (another == 'Y') {
+            status=1;
+            numCount = 0;
+            while (numCount < 5) {
+                Human_game(ans);
+                numCount++;
+            }
+        } else if (another == 'N') {
+            status=1;
+            exit(0);
+        } else {
+            cout << "I don't understand." << endl;
+        }
+    }
+    cout<<"All your attempts are being used! You lose! Bye!"<<endl;
 }
